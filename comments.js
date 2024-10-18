5a691f29109d246b8244baedd60fb6b46835dfab
Ralph Rivas <94195770+ralphrivas@users.noreply.github.com>
2024-10-18 17:10:37 +0000

    Copilot third  commit

--- comments.js
+++ comments.js
@@ -1,22 +1,60 @@
-// create web server
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-
-let comments = [];
-let id = 0;
-
+//Create Web Server with Express
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var comments = require('./comments.json');
+var path = require('path');
+var port = process.env.PORT || 3000;
 app.use(bodyParser.json());
-
-app.get('/comments', (req, res) => {
-    res.send(comments);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+//Get all comments
+app.get('/comments', function(req, res) {
+  res.json(comments);
+});
+//Get comment by id
+app.get('/comments/:id', function(req, res) {
+  var id = req.params.id;
+  var comment = comments.filter(function(comment) {
+    return comment.id == id;
+  });
+  res.json(comment[0]);
+});
+//Post a comment
+app.post('/comments', function(req, res) {
+  var comment = req.body;
+  comments.push(comment);
+  fs.writeFile('./comments.json', JSON.stringify(comments, null, 4), function(err) {
+    console.log(err);
+  });
+  res.json(comments);
+});
+//Delete a comment
+app.delete('/comments/:id', function(req, res) {
+  var id = req.params.id;
+  comments = comments.filter(function(comment) {
+    return comment.id != id;
+  });
+  fs.writeFile('./comments.json', JSON.stringify(comments, null, 4), function(err) {
+    console.log(err);
+  });
+  res.json(comments);
+});
+//Update a comment
+app.put('/comments/:id', function(req, res) {
+  var id = req.params.id;
+  var newComment = req.body;
+  comments = comments.map(function(comment) {
+    return comment.id == id ? newComment : comment;
+  });
+  fs.writeFile('./comments.json', JSON.stringify(comments, null, 4), function(err) {
+    console.log(err);
+  });
+  res.json(comments);
+});
+app.listen(port, function() {
+  console.log('Server is running on port ' + port);
 });
 
-app.post('/comments', (req, res) => {
-    const comment = req.body;
-    comment.id = id++;
-    comments.push(comment);
-    res.send(comment);
-});
 
-app.listen(3000);
