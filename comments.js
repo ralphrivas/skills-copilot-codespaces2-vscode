24687e731ad62b9b7315c6d9caede82fb55f44f8
Ralph Rivas <94195770+ralphrivas@users.noreply.github.com>
2024-10-18 17:21:47 +0000

    Update  commit

--- comments.js
+++ comments.js
@@ -1,1 +1,21 @@
-// Create Web Server with Node.js
+// Create Web Server with Node.js// Create Web Server with Node.js
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+process.on('uncaughtException', (err) => {
+    console.error('There was an uncaught error', err);
+    process.exit(1); // Exit with a failure code
+});
